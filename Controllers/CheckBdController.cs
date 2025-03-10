1fec45fb4f9c46ade67c437768eb1fdf64e8e0ca
PaolaOrtiz0320 <asiliker@gmail.com>
2025-03-10 13:25:33 -0600

    Primer commit

---
+++ Controllers/CheckBdController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+// ----------
+using apiRESTCheckBd.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+// ----- 
+namespace apiRESTCheckBd.Controllers
+{
+    public class CheckBdController : ApiController
+    {
+        //Creacion y configuracion del endpoint
+        [HttpPost]
+        [Route("tads/checkbd")]
+        public clsApiStatus checkBd()
+        {
+            //Definicion de objetos de salida 
+            clsApiStatus objRespuesta = new clsApiStatus();
+            JObject jsonResp = new JObject();
+            //----------------------------------
+
+            // Creacion del Objeto clsCheckBd
+            clsCheckBd objCheckBd = new clsCheckBd();
+
+            //Ejecucion del checkBD
+            objCheckBd.checkBd();
+            //Configuracion del objeto de salida
+            objRespuesta.ban = objCheckBd.ban;
+            if (objRespuesta.ban == 1)
+                objRespuesta.statusExec = true;
+            else
+                objRespuesta.statusExec = false;
+
+            objRespuesta.msg = objCheckBd.statusMsg;
+            jsonResp.Add("msgData", objCheckBd.statusMsg);
+            objRespuesta.datos = jsonResp;
+
+
+            //------------------------------
+            //Retorno del objeto de salida
+            return objRespuesta;
+        }
+
+
+
+
+    }
+}
---
+++ Models/clsCheckBd.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace apiRESTCheckBd.Models
+{
+    public class clsCheckBd
+    {
+        // Definición de atributos
+        private string cadConn = ConfigurationManager.ConnectionStrings["bdControl_Acceso"].ConnectionString;
+        public string statusMsg;
+        public int ban;
+        // Definición de métodos (chequeo de conexión a MySql)
+        public void checkBd()
+        {
+            try
+            {
+                // Prueba de conexión a MySql
+                MySqlConnection cnn = new MySqlConnection(cadConn);
+                // -----------
+                cnn.Open();
+                cnn.Close();
+                // -----------
+                // Conexión exitosa
+                // configuración de bandera y mensaje de salida
+                ban = 1;
+                statusMsg = "Conexion exitosa (MySql) control_acceso";
+                // -------------------------
+            }
+            catch (Exception ex)
+            {
+                // -----------
+                // Conexión exitosa
+                // configuración de bandera y mensaje de salida
+                ban = 0;
+                statusMsg = ex.Message.ToString();
+                // -------------------------
+            }
+        }
+
+
+    }
+}
